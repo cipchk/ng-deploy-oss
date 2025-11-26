d55e05abe267e0a6c61f7994128db774ec1fa7b1
cipchk <cipchk@qq.com>
2025-11-26 16:27:58 +0800

    chore: build angular to v21

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,16 @@
 /* eslint-env node */
 const eslint = require("@eslint/js");
+const { defineConfig } = require("eslint/config");
 const tseslint = require("typescript-eslint");
 
-module.exports = tseslint.config({
+module.exports = defineConfig([{
   files: ["**/*.ts"],
   extends: [
     eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    ...tseslint.configs.stylistic,
+    tseslint.configs.recommended,
+    tseslint.configs.stylistic,
   ],
   rules: {
     "@typescript-eslint/no-explicit-any": "off"
   }
-});
+}]);
